de0f5596b757c6c424331778434d691cd074d6c0
Jonathan Apodaca <jonathan.apodaca@colostate.edu>
2015-10-05 09:25:37 -0600

    Switched to nom 'xhr' package to handle XHR

--- src/client.js
+++ src/client.js
@@ -1,21 +1,39 @@
+import xhr from 'xhr'
+
 //
 // creates an object-based API based on the passed in 'spec'
-// for each leaf in the spec, attache a function returned by the makeMethodClientFactory
+// for each leaf in the spec, attach a function returned by the makeMethodClientFactory
 // spec follows the structure of jDir found in index.js
 //
-function makeFromSpec(spec, makeMethodClientFactory, currObj = '') {
+// example input: [ 'function1', { subObject: [ 'function2' ] }, 'function3' ]
+//
+// output: {
+//	function1: makeMethodClientFactory('function1'),
+//	subObject: {
+// 		function2: makeMethodClientFactory('subObject.function1')
+// 	},
+// 	function2: makeMethodClientFactory('function2')
+// }
+//
+function traverseApiSpec(spec, makeMethodClientFactory, currObj = '') {
 	let api = { }
 	for (let fn of spec) {
 		let t = typeof fn
 		switch (t) {
 		case 'string':
+			//
+			// leaf
+			//
 			let fnPath = currObj == '' ? fn : currObj + '.' + fn
 			api[fn] = makeMethodClientFactory(fnPath)
 			break
 		case 'object':
+			//
+			// loop over members and recurse
+			//
 			for (let mbr in fn) {
 				let objPath = currObj == '' ? mbr : currObj + '.' + mbr
-				api[mbr] = makeFromSpec(fn[mbr], makeMethodClientFactory, objPath)
+				api[mbr] = traverseApiSpec(fn[mbr], makeMethodClientFactory, objPath)
 			}
 			break
 		}
@@ -26,31 +44,24 @@ function makeFromSpec(spec, makeMethodClientFactory, currObj = '') {
 //
 // Promise XHR
 //
-function xhr(method, url, data) {
+function xhrp(method, url, data) {
 	return new Promise((yes, no) => {
-		let xhr = new XMLHttpRequest()
-		xhr.open(method, url, true)
+		xhr({
+			method: method,
+			body: JSON.stringify(data),
+			uri: url,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}, (err, resp, body) => {
+			if (err) return no(err)
 
-		if (method == 'POST') {
-			xhr.setRequestHeader('Content-type', 'application/json')
-			xhr.send(data ? JSON.stringify(data) : undefined)
-		} else
-			xhr.send()
-
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4)
-				if (xhr.status == 200) {
-					try {
-						yes(JSON.parse(xhr.responseText))
-					} catch (e) {
-						no({ error: 'Invalid data recieved from API call: ' + url })
-					}
-				} else {
-					let err = { status: xhr.status, error: xhr.responseText }
-					try { err = JSON.parse(xhr.responseText) } catch (e) { }
-					no(err)
-				}
-		}
+			try {
+				yes(JSON.parse(body))
+			} catch (e) {
+				no({ error: 'Invalid data recieved from API call: ' + url })
+			}
+		})
 	})
 }
 
@@ -59,11 +70,11 @@ function xhr(method, url, data) {
 //
 export default async function apiClient(base) {
 	function invoke(nm, ...args) {
-		return xhr('POST', base + '/' + nm, args)
+		return xhrp('POST', base + '/' + nm, args)
 	}
 
-	let spec = await xhr('GET', base)
-	let api = makeFromSpec(spec, function(fn) {
+	let spec = await xhrp('GET', base)
+	let api = traverseApiSpec(spec, function(fn) {
 		return function(...args) {
 			return invoke(fn, ...args)
 		}
